db604dde17e59a140b293b28d0928c56620b1760
LAPTOP-1OLM9RQ5\PC-TAKUMA <ikuta-takuma@amami-joho.ac.jp>
2026-05-18 10:31:33 +0900

    盤面の描画

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
-﻿#pragma warning(push)
+﻿#define DX_NON_USING_UNICODE_FUNCTION
+
+#pragma warning(push)
 #pragma warning(disable: 4828 4010)
 #include "DxLib.h"
 #pragma warning(pop)
+
+#define WIDTH 13
+#define HEIGHT 15
+
+char board[HEIGHT][WIDTH + 1] = {
+    "############",
+    "#          #",
+    "# ######## #",
+    "# ######## #",
+    "# ######## #",
+    "#          #",
+    "#          #",
+    "#          #",
+    "#          #",
+    "#          #",
+    "#          #",
+    "#          #",
+    "#    O     #",
+    "#    ===   #",
+    "############"
+};
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -21,6 +44,27 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         ClearDrawScreen();
 
         DrawString(10, 10, L"Hello, DXLib!", GetColor(255, 255, 255));
+
+        int cellSize = 20;
+        int offsetX = 100;
+        int offsetY = 50;
+
+        for (int y = 0; y < HEIGHT; y++)
+        {
+            for (int x = 0; x < WIDTH; x++)
+            {
+                char c = board[y][x];
+
+                wchar_t str[2] = { (wchar_t)c, L'\0' };
+
+                DrawString(
+                    offsetX + x * cellSize,
+                    offsetY + y * cellSize,
+                    str,
+                    GetColor(255, 255, 255)
+                );
+            }
+        }
 
         ScreenFlip();
     }
