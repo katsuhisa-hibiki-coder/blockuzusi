cc9b1aeb82ee0f2aba1c3e6a2236f6e9b30819ef
LAPTOP-1OLM9RQ5\PC-TAKUMA <ikuta-takuma@amami-joho.ac.jp>
2026-06-03 11:51:29 +0900

    効果音(キーで鳴るようにしています)

--- main.cpp
+++ main.cpp
@@ -23,13 +23,10 @@ char board[HEIGHT][WIDTH + 1] = {
     "             ",
     "      O      ",
     "             ",
-    "             "
+    "     ===     "
 };
 
-int berX = 5;
-int berY = 14;
-
-int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     ChangeWindowMode(TRUE);
     SetGraphMode(640, 480, 32);
@@ -40,58 +37,54 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
         return -1;
     }
 
+    //効果音
+    int hitSE = LoadSoundMem(_T("hit.mp3"));
+    int startSE = LoadSoundMem(_T("start.mp3"));//スタート音
+    int missSE = LoadSoundMem(_T("miss.mp3"));//ミス音
+
+
     SetDrawScreen(DX_SCREEN_BACK);
 
-    // パワーアップアイテム
-    float itemX = 0.0f, itemY = 0.0f;
-    bool itemActive = false;
-    int itemTimer = 0;
-
-    // スコア管理
-    int score = 0;
-
-    // ライフ管理
-    int life = 3;
-
-    // キーの1つ前の状態を記憶
-    bool prevKeyA = false;
-    bool prevKeyL = false;
-    bool prevKeyS = false;
+    int prevSpace = 0;
+    int prevS = 0;
+    int prevM = 0;
 
     while (ProcessMessage() == 0 &&
         CheckHitKey(KEY_INPUT_ESCAPE) == 0)
     {
         ClearDrawScreen();
 
-        // 左移動
-        if (CheckHitKey(KEY_INPUT_LEFT) && berX > -3) {
-            berX--;
-            WaitTimer(100);
+        // 効果音判定
+        //ヒット音(SPACE)
+        int nowSpace = CheckHitKey(KEY_INPUT_SPACE);
+
+        if (nowSpace == 1 && prevSpace == 0)
+        {
+            PlaySoundMem(hitSE, DX_PLAYTYPE_BACK);
         }
 
-        // 右移動
-        if (CheckHitKey(KEY_INPUT_RIGHT) && berX < WIDTH) {
-            berX++;
-            WaitTimer(100);
+        prevSpace = nowSpace;
+
+        // スタート音(Sキー)
+        int nowS = CheckHitKey(KEY_INPUT_S);
+
+        if (nowS == 1 && prevS == 0)
+        {
+            PlaySoundMem(startSE, DX_PLAYTYPE_BACK);
         }
 
-        // スコア管理（Aキーでテスト加算）
-        bool currentKeyA = CheckHitKey(KEY_INPUT_A);
-        if (currentKeyA && !prevKeyA) {
-            score += 10;
+        prevS = nowS;
+
+        // ミス音(Mキー)
+        int nowM = CheckHitKey(KEY_INPUT_M);
+
+        if (nowM == 1 && prevM == 0)
+        {
+            PlaySoundMem(missSE, DX_PLAYTYPE_BACK);
         }
-        prevKeyA = currentKeyA;
 
 
-        // ライフ管理（Lキーでテスト減少）
-        bool currentKeyL = CheckHitKey(KEY_INPUT_L);
-        if (currentKeyL && !prevKeyL) {
-            if (life > 0) {
-                life--;
-            }
-        }
-        prevKeyL = currentKeyL;
-
+        prevM = nowM;
 
         // マスサイズ
         int cellSize = 32;
@@ -99,47 +92,6 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
         // 画面中央配置
         int offsetX = (640 - WIDTH * cellSize) / 2;
         int offsetY = (480 - HEIGHT * cellSize) / 2;
-
-        SetDrawScreen(DX_SCREEN_BACK);
-
-        // パワーアップアイテム（Sキーでテスト落下）
-        bool currentKeyS = CheckHitKey(KEY_INPUT_S);
-        if (currentKeyS && !prevKeyS && !itemActive)
-        {
-            itemActive = true;
-            itemX = (float)(offsetX + 6 * cellSize + cellSize / 2);
-            itemY = (float)offsetY;
-        }
-        prevKeyS = currentKeyS;
-
-
-        if (itemActive)
-        {
-            itemY += 3.0f;
-            if (itemY > 480) itemActive = false;
-
-            float barY = (float)(offsetY + berY * cellSize);
-            float barLeft = (float)(offsetX + berX * cellSize);
-            float barRight = (float)(offsetX + (berX + 3) * cellSize);
-
-            // パワーアップアイテム
-            if (itemTimer > 0)
-            {
-                barLeft -= 16.0f;
-                barRight += 16.0f;
-            }
-
-            if (itemY >= barY && itemY <= barY + cellSize && itemX >= barLeft && itemX <= barRight)
-            {
-                itemActive = false;
-                itemTimer = 300;
-            }
-        }
-
-        if (itemTimer > 0)
-        {
-            itemTimer--;
-        }
 
         // 描画
         for (int y = 0; y < HEIGHT; y++)
@@ -156,50 +108,55 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
                 // 紫ブロック
                 if (c == 'B')
                 {
-                    DrawBox(left, top, right, bottom, GetColor(255, 0, 255), TRUE);
+                    DrawBox(
+                        left,
+                        top,
+                        right,
+                        bottom,
+                        GetColor(255, 0, 255),
+                        TRUE
+                    );
                 }
+
                 // 緑ブロック
                 else if (c == 'G')
                 {
-                    DrawBox(left, top, right, bottom, GetColor(0, 255, 0), TRUE);
+                    DrawBox(
+                        left,
+                        top,
+                        right,
+                        bottom,
+                        GetColor(0, 255, 0),
+                        TRUE
+                    );
                 }
+
                 // ボール
                 else if (c == 'O')
                 {
-                    DrawCircle(left + cellSize / 2, top + cellSize / 2, 5, GetColor(255, 255, 255), TRUE);
+                    DrawCircle(
+                        left + cellSize / 2,
+                        top + cellSize / 2,
+                        5,
+                        GetColor(255, 255, 255),
+                        TRUE
+                    );
+                }
+
+                // バー
+                else if (c == '=')
+                {
+                    DrawBox(
+                        left,
+                        top + 10,
+                        right,
+                        bottom - 10,
+                        GetColor(0, 255, 255),
+                        TRUE
+                    );
                 }
             }
         }
-
-        // バー描画
-        for (int i = 0; i < 3; i++) {
-            int left = offsetX + (berX + i) * cellSize;
-            int top = offsetY + berY * cellSize;
-            int right = left + cellSize - 2;
-            int bottom = top + cellSize - 2;
-
-            // パワーアップアイテム
-            int extend = (itemTimer > 0) ? 16 : 0;
-            unsigned int barColor = (itemTimer > 0) ? GetColor(255, 200, 220) : GetColor(0, 255, 255);
-
-            int finalLeft = left - (i == 0 ? extend : 0);
-            int finalRight = right + (i == 2 ? extend : 0);
-
-            DrawBox(finalLeft, top + 10, finalRight, bottom - 10, barColor, TRUE);
-        }
-
-        // パワーアップアイテム描画
-        if (itemActive)
-        {
-            DrawCircle((int)itemX, (int)itemY, 8, GetColor(255, 50, 150), TRUE);
-            DrawCircle((int)itemX, (int)itemY, 6, GetColor(255, 255, 255), TRUE);
-        }
-
-        // スコア管理
-        DrawFormatString(20, 20, GetColor(255, 255, 255), L"SCORE: %d", score);
-
-        // ライフ管理
-        DrawFormatString(20, 45, GetColor(255, 255, 255), L"LIFE: %d", life);
 
         ScreenFlip();
     }
